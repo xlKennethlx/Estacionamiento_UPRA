d918570549e605bd37eb330629718c01a5d81880
MonsterGonzalez <gabrielgonzalez@Gabriels-MacBook-Pro.local>
2017-08-31 01:30:05 -0400

    logo para la aplicación
    -logo listo
    - detalles para el splash screen

--- app/src/main/java/com/example/kenneth/estacionamiento_upra/Main.java
+++ app/src/main/java/com/example/kenneth/estacionamiento_upra/Main.java
@@ -1,13 +1,24 @@
 package com.example.kenneth.estacionamiento_upra;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sabithpkcmnr.splashscreen.HomeActivity;
+
 public class Main extends AppCompatActivity {
+
+    //private static int SPLASH_TIME_OUT = 4000; // 4 seconds of time where the splash screeen ends   
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
+
