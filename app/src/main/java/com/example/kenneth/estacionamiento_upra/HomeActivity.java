965f86f0a52aa9eed915f8f173067c80ce13da62
MonsterGonzalez <gabrielgonzalez@Gabriels-MacBook-Pro.local>
2017-09-15 15:46:48 -0400

    Splash screen animation
    -rotating icon animation
    -title bar removed

--- app/src/main/java/com/example/kenneth/estacionamiento_upra/HomeActivity.java
+++ app/src/main/java/com/example/kenneth/estacionamiento_upra/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.kenneth.estacionamiento_upra;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.example.kenneth.estacionamiento_upra.R;
 
@@ -10,6 +12,8 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //remove the tittlebar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home);
     }
 }
--- app/src/main/java/com/example/kenneth/estacionamiento_upra/Main.java
+++ app/src/main/java/com/example/kenneth/estacionamiento_upra/Main.java
@@ -1,5 +1,6 @@
 package com.example.kenneth.estacionamiento_upra;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,17 +9,23 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.TimerTask;
 import java.util.Timer;
 
 public class Main extends AppCompatActivity {
-    private static final long SPLASH_SCREEN_DELAY = 2500;
+    private static final long SPLASH_SCREEN_DELAY = 3200;
     Context context = this;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
         TimerTask task = new TimerTask() {
             @Override
@@ -29,6 +36,16 @@ public class Main extends AppCompatActivity {
                     finish();
             }
         };
+
+
+
+        ImageView icon = (ImageView)findViewById(R.id.imageView);
+    // This is where the icon picture is been animated, it will expand and rotate
+        icon.animate()
+                .scaleX(1.5f)
+                .scaleY(1.5f)
+                .rotationBy(720)
+                .setDuration(2000); //2 second duration
         // Simulate a long loading process on application startup.
         Timer timer = new Timer();
         timer.schedule(task, SPLASH_SCREEN_DELAY);
