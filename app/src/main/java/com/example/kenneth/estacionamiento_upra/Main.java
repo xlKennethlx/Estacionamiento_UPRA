426e8b05aeab4fb1f4db575bbada46f3908fb276
MonsterGonzalez <gabrielgonzalez@Gabriels-MacBook-Pro.local>
2017-12-21 11:31:32 -0400

    hope this does not break it

--- app/src/main/java/com/example/kenneth/estacionamiento_upra/Main.java
+++ app/src/main/java/com/example/kenneth/estacionamiento_upra/Main.java
@@ -1,5 +1,23 @@
-package com.example.kenneth.estacionamiento_upra;
+package com.example.kenneth.estacionamiento_upra;  
+        import android.content.Intent; 
+        import android.os.Handler;
+         import android.support.v7.app.AppCompatActivity;
+         import android.os.Bundle;
+          public class Main extends AppCompatActivity {  
+    private static int SPLASH_TIME_OUT = 4000; // 4 seconds of time where the splash screeen ends   
+//   @Override     protected void onCreate(Bundle savedInstanceState) { 
+//      super.onCreate(savedInstanceState); 
+// setContentView(R.layout.activity_main);  
+// necesarry code for the spash screen 
+// new Handler().postDelayed(new Runnable(){ 
+//    @Override             public void run(){ 
+//   Intent homeIntent = new Intent(Main.this, HomeActivity.class); 
+//            startActivity(homeIntent);                 finish(); 
+//  } 
+//     },SPLASH_TIME_OUT);     }
+//  }
 
+<<<<<<< Updated upstream
 
 import android.content.Context;
 import android.content.Intent;
@@ -54,3 +72,5 @@ public class Main extends AppCompatActivity {
 }
 
 
+=======
+>>>>>>> Stashed changes
